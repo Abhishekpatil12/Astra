77d119a5979c3b9463c04aaed85347c8306595d0
Niraj Amrutkar <>
2023-12-02 18:03:51 +0530

    dynamic data link fetching

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const ejs = require('ejs');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("views"));
 
@@ -10,14 +12,23 @@ const { ref, get } = require("firebase/database");
 const db = require('./db/db');
 
 app.get('/data', async function (req, res) {
-    const rootRef = ref(db, '/Astra');
-    const data = await get(rootRef);
-    res.send(data);
+    const rootRef = ref(db, '/astras');
+    const astrasData = await get(rootRef);
+    const astrasArray = Object.values(astrasData.val());
+    res.render('show', { astras: astrasArray });
+});
+
+app.get('/data/:astraname', async function(req, res){
+    const keyToRetrieve = req.params.astraname.toLowerCase();
+    const astraRef = ref(db, `/astras/${keyToRetrieve}`);
+    const astraData = await get(astraRef);
+    const astra = astraData.val();
+    res.render('astra', {astra});
 });
 
 app.get('/', function(req, res){
-    res.render('index.html');
-})
+    res.render('index');
+});
 
 app.listen(3000, function(){
     console.log("Server stated at 3000");
